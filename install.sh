09aef3e1b6d06c7e5d96bfbf5abde4d96fe10f8b
baronTommy <imatomiyuichi+github@gmail.com>
2017-05-14 20:39:04 +0900

    🐚  install.sh 修正

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 
 mkdir -p $XDG_CONFIG_HOME
 
+# hammerspoon
 ln -s ~/dotfiles/.hammerspoon ~/.
-ln -s ~/dotfiles/.bash_profile ~/.
+
+# nvim
 ln -s ~/dotfiles/nvim $XDG_CONFIG_HOME/.
+
+# bash
+ln -s ~/dotfiles/.bash_profile ~/.
+
+# new-filet-o-fish
+git clone https://github.com/baronTommy/new-filet-o-fish.git ~/new-filet-o-fish
+ln -s ~/new-filet-o-fish/hammerspoon_helper ~/.hammerspoon/.
