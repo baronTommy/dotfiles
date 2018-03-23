df935cb393c8737fc3e403bfa014185490b4e15f
baronTommy <imatomiyuichi+github@gmail.com>
2018-03-23 14:44:56 +0900

    greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,3 +1,12 @@
 function fish_greeting
-    echo (whoami) !!
+  echo ''
+  echo 'Current time is ...'
+  echo '  '(date +'%Y-%m-%d %H:%M:%S')
+  echo ''
+  echo 'Current Directory is ...'
+  echo '  '(pwd)
+  echo ''
+  echo 'Login User is ...'
+  echo '  '(whoami)
+  echo ''
 end
