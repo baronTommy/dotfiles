a7581202bd39a45eafb9cac1250be5ed974bdbc5
baronTommy <imatomiyuichi+github@gmail.com>
2018-03-23 16:35:05 +0900

    greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,12 +1,30 @@
+function __echo --argument v prefixFlg
+  if test -z $prefixFlg;
+    set prefixFlg false
+  end
+
+  set -l prefix \t
+  if test $prefixFlg = true;
+    echo -n $prefix
+  end
+
+  echo $v
+end
+
+function __breakLine
+  __echo '' false
+end
+
 function fish_greeting
-  echo ''
-  echo 'Current time is ...'
-  echo '  '(date +'%Y-%m-%d %H:%M:%S')
-  echo ''
-  echo 'Current Directory is ...'
-  echo '  '(pwd)
-  echo ''
-  echo 'Login User is ...'
-  echo '  '(whoami)
-  echo ''
+  __echo 'Current time is ...'
+  __echo (date +'%Y-%m-%d %H:%M:%S') true
+  __breakLine
+
+  __echo 'Current Directory is ...'
+  __echo (pwd) true
+  __breakLine
+
+  __echo 'Login User is ...'
+  __echo (whoami) true
+  __breakLine
 end
