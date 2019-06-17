8c78483bf95e862981f145b4fc14c3a4ff78e564
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-06-17 17:58:16 +0900

    まぁ あれ

--- fish/functions/fish_user_key_bindings.fish
+++
@@ -1,4 +0,0 @@
-function fish_user_key_bindings
-  # bindの場合$statusが取得出来ない
-  # bind \ce hi
-end
--- fish/functions/hi.fish
+++
@@ -1,8 +0,0 @@
-function hi
-  echo 'hi'
-  # set -g foo 1
-  # echo 'hello'
-  # echo -e '\n'
-  # commandline -f repaint
-  # return 1
-end
